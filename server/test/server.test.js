f3475d20c0efa133fb14cb90b8a51cad5142dd7e
Raygon <rockyracy@gmail.com>
2018-09-30 09:50:55 +0545

    testing the delete case

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -102,3 +102,40 @@ it('should retun 404 for non-object ids',(done)=>{
 .end(done);
 });
 });
+describe('Delete/todos:id',()=>{
+    it ('should remove a todo',(done)=>{
+        // var id= new ObjectID();
+        // var hexId= todos[0]._id.toHexString();
+        request(app)
+        .delete(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res)=>{
+expect ((res.body.todo._id).toBe(hexId));
+        })
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+            Todo.findById(hexId).then((todo)=>{
+expect(todo).toNotExist();
+done();
+            }).catch ((e)=>done());
+
+        });
+    });
+     it('should return 404 if todo not found',(done)=>{
+        var _id = new ObjectID()
+        request(app)
+        .delete(`/todos/${todos}._id.toHexString()`)
+        .expect(404)
+        .end(done);
+        
+   });
+    it('should return 404 if object id is invalid',(done)=>{
+       request(app)
+        .delete('/todos/123abc')
+.expect(404)
+.end(done);
+});
+});
+  
